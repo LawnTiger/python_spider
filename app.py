27a315f389149b5b2caef31fb8165aee8ef713b7
Buzzyu <3039829493@qq.com>
2017-02-14 23:57:20 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # coding=utf-8
-
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from selenium import webdriver
@@ -10,4 +9,16 @@ import re
 
 def main():
     driver = webdriver.PhantomJS()
-    driver.get()
+    driver.get('') # edit the url here
+    html = BeautifulSoup(driver.page_source, 'lxml')
+    jobUrls = bsObj.findAll('a', {'href': re.compile('')})
+    
+    url = []
+    for url in jobUrls:
+        info.append() = getInfo(url)
+
+def getInfo():
+    pass
+
+if __name__ == '__main__':
+    main()

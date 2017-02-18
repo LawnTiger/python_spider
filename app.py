9fa4975b2737f7ace8056bcc86e2694500dc8659
LawnTiger <3039829493@qq.com>
2017-02-18 23:56:15 +0800

    update app.py

--- app.py
+++ app.py
@@ -10,19 +10,27 @@ import re
 driver = webdriver.PhantomJS()
 
 def main():
-    driver.get('') # edit the url here
-    html = BeautifulSoup(driver.page_source, 'lxml')
-    jobUrls = html.findAll('a', {'href': re.compile('')})
-    
+    driver.get('http://search.51job.com/list/030200%252C00,000000,0000,00,9,07,php,2,1.html?lang=c&degreefrom=99&stype=1&workyear=99&cotype=99&jobterm=99&companysize=99&radius=-1&address=&lonlat=&postchannel=&list_type=&ord_field=&curr_page=&dibiaoid=0&landmark=&welfare=') # edit the url here
+    html = BeautifulSoup(driver.page_source, 'html5lib')
+    jobUrls = html.findAll('a', {'href': re.compile('http://jobs.51job.com/guangzhou*')})
+
     info = []
     for url in jobUrls:
-        info.append(getInfo(url))
+#        print(url['href'])
+        info.append(getInfo(url['href']))
+    print(info)
 
 def getInfo(url):
     driver.get(url)
-    html = BeautifulSoup(driver.page_source, 'lxml')
-    requirement = html.findAll('div', {'id': re.compile('')})
-    return requirement
+    html = BeautifulSoup(driver.page_source, 'html5lib')
+    requirement = html.find('div', {'class': re.compile('bmsg job_msg inbox')})
+
+    tmp = ''
+    for requirement1 in requirement:
+        tmp += requirement1.string
+        
+    return tmp
+
 
 if __name__ == '__main__':
     main()

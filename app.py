0ffb645ba4a820058d8e09f3106fdbf2e605ed22
LawnTiger <3039829493@qq.com>
2017-02-21 23:14:16 +0800

    fix正则

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def main():
 #    driver.get('http://search.51job.com/list/030200%252C00,000000,0000,00,9,07,php,2,1.html?lang=c&degreefrom=99&stype=1&workyear=99&cotype=99&jobterm=99&companysize=99&radius=-1&address=&lonlat=&postchannel=&list_type=&ord_field=&curr_page=&dibiaoid=0&landmark=&welfare=') # exemple
     driver.get('') # add the url here
     html = BeautifulSoup(driver.page_source, 'html5lib')
-    jobUrls = html.findAll('a', {'href': re.compile('http://jobs.51job.com/guangzhou*')})
+    jobUrls = html.findAll('a', {'href': re.compile('http://jobs.51job.com/[\w-]*/\d')})
 
     info = []
     for url in jobUrls:

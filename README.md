c40b10d43df28a8286ddac6c4c2136d7f2247717
余灿豪 <3039829493@qq.com>
2017-03-21 18:15:25 +0800

    Add get_jiaoyi.py

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# get_requirement
+# 1.get_requirement
 #### 爬取某程无忧网企业任职要求
-#### 很多时候我们觉得自己学的东西够多了，但只要你上招聘网查看招聘条件，你对自己的认识又会回归正道了
-修改app.py中driver.get()内参数，填入筛选条件后的地址即可
+#### 有时我们觉得自己学的东西够多了，但只要你上招聘网查看招聘条件，你就清醒了
+#### python3 使用 beautiful4 和 selenium ,加上 PhantomJS 无头浏览器完成项目
 
-### 使用 beautiful4 和 selenium ,加上 PhantomJS 浏览器完成项目
+# 2.get_jiaoyi
+#### 爬取某交易平台数据
+#### python2 用 urllib 发起请求，pymysql连接数据库，多线程抓取
+#### 感谢 `Andrew_liu` 写的[教程](http://www.jianshu.com/p/c3dbf8294c33)的指导
---
+++ get_jiaoyi.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python2
+# -*- coding:utf-8 -*-
+
+import urllib, urllib2
+import cookielib
+import ssl
+import pymysql
+import json
+import datetime
+import threading
+import socket
+
+
+TIMEOUT = 5
+THREAD_COUNT = 100
+
+socket.setdefaulttimeout(TIMEOUT) # 设置超时时间
+ssl._create_default_https_context = ssl._create_unverified_context # 允许不安全访问
+
+
+conn = pymysql.connect(host='localhost',
+                       user='root',
+                       password='',
+                       db='test',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
+
+# 登录、验证码
+code_url = ""
+login_url = ""
+
+# 请求数据的 url
+u = ['', '', '']
+
+
+def simulation_login() :
+    cookie = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+    urllib2.install_opener(opener)
+
+    form_data = urllib.urlencode({
+        "loginName": '',
+        "password": '',
+    })
+    req = urllib2.Request(code_url, data = form_data, headers = structure_headers())
+    result = urllib2.urlopen(req, timeout = TIMEOUT)
+    content = result.read()
+    print(str(content))
+    word = input('>>')
+    form_data = urllib.urlencode({
+        "loginName": '',
+        "password": '',
+        'code': word
+    })
+    req = urllib2.Request(login_url, data = form_data, headers = structure_headers())
+    result = urllib2.urlopen(req, timeout = TIMEOUT)
+    content = result.read()
+    print(str(content))
+
+def get_data(c_id):
+    if c_id % 500 == 0:
+        print('-> ', c_id) # 输出进度
+    form_data = urllib.urlencode({
+        "pageIndex": '1',
+        "pageSize": '100000',
+        'accountID': c_id
+    })
+    data = list()
+    for i in [0, 1, 2]:
+        try:
+            req = urllib2.Request(u[i], data = form_data, headers = structure_headers())
+            result = urllib2.urlopen(req, timeout = TIMEOUT)
+            result = str(result.read())
+        except:
+            result = 'time out'
+            print(c_id, i, result)
+        finally:
+            data.append(result)
+    return data
+
+def structure_headers():
+    user_agent = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/38.0.2125.111 Safari/537.36")
+
+    request_header = {
+        "User-Agent": user_agent, # 伪装成浏览器访问
+        "X-Requested-With": "XMLHttpRequest",
+    }
+    return request_header
+
+def get_ids():
+    try:
+        with conn.cursor() as cursor:
+            sql = "select id from customers order by id asc"
+            cursor.execute(sql)
+            ids = cursor.fetchall()
+    finally:
+        pass
+    return ids
+
+def deal(ids, filename):
+    for c_id in ids:
+        c_id = c_id['id']
+        data = get_data(c_id)
+        f = open(filename, 'a')
+        try:
+            f.write('\n'+str(c_id)+'|||'+json.dumps(data))
+        except:
+            print(c_id, 'write error')
+        finally:
+            f.close()
+
+
+def main():
+    simulation_login()
+    print(datetime.datetime.now().strftime('%X')) # 开始时间
+
+    ids = get_ids()
+    threads = []
+
+    # id分组
+    id_group = [ids[i:i+THREAD_COUNT] for i in xrange(0, len(ids), THREAD_COUNT)]
+    # 文件名组
+    filename_group = ['d'+str(i)+'.txt' for i in xrange(THREAD_COUNT)]
+
+    for i in xrange(THREAD_COUNT):
+        threads.append(threading.Thread(target=deal, args=(id_group[i], filename_group[i])))
+
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    conn.close()
+    print(datetime.datetime.now().strftime('%X')) # 结束时间
+
+
+if __name__ == '__main__':
+    main()

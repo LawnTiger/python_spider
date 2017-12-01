931f48d8212d03e996cb4f26fd767463c4522de5
余灿豪 <3039829493@qq.com>
2017-12-01 18:43:43 +0800

    update get juming

--- wechat_prevent/recheck_juming.py
+++ wechat_prevent/recheck_juming.py
@@ -2,12 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import requests
-from bs4 import BeautifulSoup
 import queue
 import threading
 import json
 import pymysql
-import time
 
 
 get_url = 'http://www.juming.com/ykj/?api_sou=1&ymlx=0&jgpx=0&meiye=&page='
@@ -16,7 +14,7 @@ check_url = 'http://wx.api-export.com/api/checkdomain?key=*&url='
 domain_queue = queue.Queue()
 check_queue = queue.Queue()
 
-THREAD_COUNT = 100
+THREAD_COUNT = 300
 
 conn = pymysql.connect(host='localhost',
                        user='root',
@@ -33,30 +31,45 @@ headers = {
 }
 
 
-def get_domains(request_url, f):
-    response = requests.get(request_url, headers=headers)
-    response.encoding = 'utf-8'
-    soup = BeautifulSoup(response.text, 'html.parser')
-    domains = soup.findAll('td', {'class': 'xinbz'})
+def get_domains():
+    with conn.cursor() as cursor:
+        sql = "SELECT domain FROM `domain_171201`"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    for i in result:
+        domain_queue.put(i['domain'])
 
-    for domain in domains:
-        for inp in domain.children:
-            a = inp.find('a')
-            if a.string is not None and a.string != ' ':
-                domain_queue.put(a.string)
-                # f.write(a.string + '\n')
+    # for i in range(10):
+    #     for line in open('data/juming/recheck.txt', 'r'):
+    #         line = line.replace('\n', '')
+    #         if line is '':
+    #             continue
+    #         domain_queue.put(line)
+
+    # response = requests.get(request_url, headers=headers)
+    # response.encoding = 'utf-8'
+    # soup = BeautifulSoup(response.text, 'html.parser')
+    # domains = soup.findAll('td', {'class': 'xinbz'})
+    #
+    # for domain in domains:
+    #     for inp in domain.children:
+    #         a = inp.find('a')
+    #         if not a.string is None and a.string != ' ':
+    #             domain_queue.put(a.string)
+    #             f.write(a.string + '\n')
 
 
-def check_domain(f):
+def check_domain(f, fs):
     while True:
         domain = domain_queue.get()
         domains = {'original': domain, 'pre': 'abc.' + domain, 'rear': domain + '/abc', 'pr': 'abc.' + domain + '/abc'}
         data = dict()
         for key, domain1 in domains.items():
+            response = requests.get(check_url + domain1)
+            response.encoding = 'utf-8'
             try:
-                response = requests.get(check_url + domain1)
-                response.encoding = 'utf-8'
                 result = json.loads(response.text)
+                # fs.write(response.text)
                 status = result['code']
             except Exception as e:
                 status = -1
@@ -72,15 +85,9 @@ def save():
         data = check_queue.get()
         try:
             with conn.cursor() as cursor:
-                sql = "SELECT * FROM `domain_171201` WHERE `domain`=%s"
-                cursor.execute(sql, (data['domain'],))
-                result = cursor.fetchone()
-                if result:
-                    sql = "UPDATE domain_171201 SET original = %s , pre = %s , rear = %s , pr = %s WHERE domain = %s"
-                    cursor.execute(sql, (data['original'], data['pre'], data['rear'], data['pr'], data['domain']))
-                else:
-                    sql = "INSERT INTO domain_171201 (`domain`, `original`, `pre`, `rear`, `pr`, `d_from`) VALUES (%s, %s, %s, %s, %s, 'juming.com')"
-                    cursor.execute(sql, (data['domain'], data['original'], data['pre'], data['rear'], data['pr']))
+                # sql = "INSERT INTO `re_domain_check` (`domain`, `original`, `pre`, `rear`, `pr`, `d_from`) VALUES (%s, %s, %s, %s, %s, 'juming.com')"
+                sql = "UPDATE domain_171201 SET original = %s , pre = %s , rear = %s , pr = %s WHERE domain = %s"
+                cursor.execute(sql, (data['original'], data['pre'], data['rear'], data['pr'], data['domain']))
                 conn.commit()
         except Exception as e:
             print(e)
@@ -89,8 +96,9 @@ def save():
 
 def main():
     cf = open('data/juming/check.txt', 'a')
+    cfs = open('data/juming/check_source.txt', 'a')
     for i in range(THREAD_COUNT):
-        t = threading.Thread(target=check_domain, args=(cf,))
+        t = threading.Thread(target=check_domain, args=(cf, cfs))
         t.daemon = True
         t.start()
 
@@ -98,20 +106,20 @@ def main():
     t1.daemon = True
     t1.start()
 
-    for i in range(1, 3000):
-        if i % 50 == 0:
-            time.sleep(1)
-            print('--------------------' + str(i) + '-----------------------')
-        try:
-            with open('data/juming/domains.txt', 'a') as df:
-                get_domains(get_url + str(i), df)
-            time.sleep(0.1)
-        except Exception as e:
-            print(e)
+    get_domains()
+    # for i in range(1, 100):
+    #     if i % 50 == 0:
+    #         print('--------------------' + str(i) + '-----------------------')
+    #     try:
+    #         with open('data/juming/domains.txt', 'a') as df:
+    #             get_domains(get_url + str(i), df)
+    #     except Exception as e:
+    #         print(e)
 
     domain_queue.join()
     check_queue.join()
     cf.close()
+    cfs.close()
 
 
 if __name__ == '__main__':
